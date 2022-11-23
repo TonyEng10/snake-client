87c0dd90655934101a4e8913ecebe3d5bf8596a4
Tony Eng <tony.eng@telus.com>
2022-11-23 09:12:00 -0800

    created client.js to take connection func from play.js and rquire the func

--- play.js
+++ play.js
@@ -1,18 +1,6 @@
 // "localhost"
 const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 3000
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
+const connect = require("./client");
 
 console.log("Connecting ...");
 connect();
