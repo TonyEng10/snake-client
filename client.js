fb6ae92ecefa3844a96a8d3b750225d7ba0ca64b
Tony Eng <tony.eng@telus.com>
2022-11-23 09:44:34 -0800

    updated client.js options to move the snake around with timeouts and intervals

--- client.js
+++ client.js
@@ -8,10 +8,39 @@ const connect = function () {
   });
   
   conn.on("connect", (data) => {
-    console.log(data);
+    console.log("you are now connected");
   });
   conn.on("connect", () => {
     conn.write("Name: TE");
+
+  });
+  conn.on("connect", (data) => {
+    // setInterval(() => {
+    //   conn.write("Move: up")
+    // }, 100)
+    // setInterval(() => {
+    //   conn.write("Move: right")
+    // }, 150)
+    
+    setTimeout(() => {
+    conn.write("Move: up");
+    }, 200)
+    setTimeout(() => {
+      conn.write("Move: left");
+    }, 400)
+    setTimeout(() => {
+      conn.write("Move: down");
+    }, 600)
+    setTimeout(() => {
+      conn.write("Move: left");
+    }, 800)
+    setTimeout(() => {
+      conn.write("Move: down");
+    }, 1000)
+    setTimeout(() => {
+      conn.write("Move: left");
+    }, 1200)
+  
   });
 // interpret incoming data as text
 conn.setEncoding("utf8");
